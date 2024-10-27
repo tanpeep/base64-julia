da86aeb0f055b0ef5225ba7cb872d6eda4597a99
Sulthan Afif Althaf <sulthan.afif@ui.ac.id>
2024-10-27 10:42:56 +0700

    feat: implement base63 decoder

--- base64.jl
+++ base64.jl
@@ -32,6 +32,26 @@ module Base64
         return res
     end
 
+    function decode(str)
+        binStr = ""
+        for chr in str 
+            if chr != '='
+                val = mapEncodedCharToInt(chr)
+                binStr = binStr * Binary.intToBinStr(val, 6)
+            end
+        end
+
+        res = ""
+        i = 1
+        while i < length(binStr) 
+            val = Binary.binStrToInt(binStr[i:i+7])
+            res = res * Char(val)
+            i += 8
+        end
+
+        return res
+    end
+
     function mapIntToEncodedChar(val)
         if val == 62 
             return '+'
@@ -43,6 +63,21 @@ module Base64
             return Char(val + 71)
         else 
             return Char(val - 4)
+        end
+    end
+
+    function mapEncodedCharToInt(chr)
+        val = Int(chr)
+        if val == 43
+            return val + 19
+        elseif val == 47
+            return val + 16
+        elseif val >= 97
+            return val - 71
+        elseif val >= 65
+            return val - 65
+        else val >= 48
+            return val + 4
         end
     end
 
